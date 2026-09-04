426599951a1fde87dda00d34956b764db013872c
wissaleayate <belalawissal@gmail.com>
2026-09-04 15:27:03 +0100

    update password

--- backend/app.py
+++ backend/app.py
@@ -19,8 +19,8 @@ CORS(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True 
-app.config['MAIL_USERNAME'] = "chahinezouar02@gmail.com"
-app.config['MAIL_PASSWORD'] = 'wdrl brzs oeeh dzpc'
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")  
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = ('Drippy Store', app.config['MAIL_USERNAME'])
 
 mail = Mail(app)
--- backend/database.py
+++ backend/database.py
@@ -6,6 +6,20 @@ db = SQLAlchemy()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_DIR = os.path.join(BASE_DIR, "database")
 os.makedirs(DATABASE_DIR, exist_ok=True)
+
 DATABASE_PATH = os.path.join(DATABASE_DIR, "shoe_store.db")
 
-DATABASE_URI = "sqlite:///" + DATABASE_PATH
+# Local development → SQLite
+# Production (Render) → PostgreSQL through DATABASE_URL
+DATABASE_URI = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URI:
+    DATABASE_URI = "sqlite:///" + DATABASE_PATH
+
+# Some providers may return postgres://
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
