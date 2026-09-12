5d8d07aa6357df28bac5ed4bb40c580ed4f3b830
wissaleayate <belalawissal@gmail.com>
2026-09-12 14:49:33 +0100

    fix admin

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,5 @@
 import { useLang } from '../context/LanguageContext'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 
 export default function Categories() {
   const { t } = useLang()
